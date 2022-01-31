008b382dd4673c500c524823bdcc177c59557472
Dionovan Ramadhani <80899280+nyanboy1127@users.noreply.github.com>
2022-01-31 16:44:32 +0700

    addtorque, win lose condition

---
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float delayTime = 2f;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Ground"){
+        Invoke("ReloadScene", delayTime);    
+        }
+    }
+
+    void ReloadScene() {
+        SceneManager.LoadScene(0);
+    }
+}
